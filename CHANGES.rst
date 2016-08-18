7ca234f5566c521aa2a9e634896c89fd1a05488e
Santosh Hulisandrasrikanta <santosh.hulisandrasrikanta@ap.sony.com>
2016-08-18 15:22:36 +0530

    Added support for xdist plugin

--- CHANGES.rst
+++ CHANGES.rst
@@ -5,3 +5,8 @@ Release Notes
 **0.1.1 (2016-08-04)**
 
 * Initial release
+
+**1.1.1 (2016-08-18)**
+
+* Added support for Xdist plugin feature.
+
--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 import os
 import re
-import codecs
 
 from setuptools import setup
 
@@ -8,43 +7,43 @@ from setuptools import setup
 def get_version(filename):
 
     here = os.path.dirname(os.path.abspath(__file__))
-    f = codecs.open(os.path.join(here, filename), encoding='utf-8')
-    version_file = f.read()
+    f = open(os.path.join(here, filename))
+    version_match = f.read()
     f.close()
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
+
     if version_match:
-        return version_match.group(1)
+        return version_match
     raise RuntimeError("Unable to find version string.")
 
 
 
 setup(name='pytest-excel',
-      version=get_version('pytest_excel.py'),
+      version=get_version('version.txt'),
       description='pytest plugin for generating excel reports',
-      long_description=open('README.rst').read(),
+      long_description=unicode(open('README.rst').read(), errors='ignore'),
       author='santosh',
-      author_email='santosh.srikanta@gmail.com',
-      url='https://github.com/ssrikanta/pytest-excel',
+      author_email=u'santosh.srikanta@gmail.com',
+      url=u'https://github.com/ssrikanta/pytest-excel',
       packages=['pytest_excel'],
-      entry_points={'pytest11': ['excel = pytest_excel']},
-      install_requires=['pytest>=2.3', 'openpyxl'],
+      entry_points={'pytest11': ['excel = pytest_excel.pytest_excel']},
+      install_requires=['pytest>=2.7', 'openpyxl'],
       keywords='py.test pytest excel report',
       classifiers=[
-          'Development Status :: 1 - Alpha',
+          'Development Status :: 4 - Beta',
           'Framework :: Pytest',
           'Intended Audience :: Developers',
-          'License :: OSI Approved :: BSD License',
           'Operating System :: POSIX',
           'Operating System :: Microsoft :: Windows',
           'Operating System :: MacOS :: MacOS X',
           'Topic :: Software Development :: Testing',
+          'Topic :: Software Development :: Quality Assurance',
           'Topic :: Software Development :: Libraries',
           'Topic :: Utilities',
-          'Programming Language :: Python :: 2',
           'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
           'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5'
           'Programming Language :: Python :: Implementation :: PyPy',
       ]
       )
