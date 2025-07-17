5b12c0a120386ac072fe41614c9c16fcc715daeb
ssrikanta <santosh.srikanta@gmail.com>
2025-07-17 21:52:46 +0530

    1.8.0

--- setup.py
+++ setup.py
@@ -1,7 +1,3 @@
-import os
-import re
-import io
-
 from setuptools import setup
 
 
@@ -9,16 +5,15 @@ from setuptools import setup
 setup(name='pytest-excel',
       version='1.8.0',
       description='pytest plugin for generating excel reports',
-      long_description=io.open('README.rst', encoding='utf-8', errors='ignore').read(),
+      long_description=open('README.rst', encoding='utf-8', errors='ignore').read(),
       author='santosh',
       author_email=u'santosh.srikanta@gmail.com',
       url=u'https://github.com/ssrikanta/pytest-excel',
       license = 'MIT',
-      license_file = 'LICENSE',
       packages=['pytest_excel'],
       entry_points={'pytest11': ['excel = pytest_excel.pytest_excel']},
       install_requires=[
-          'pytest>=3.11',
+          'pytest',
           'pandas',
           'openpyxl',
       ],
@@ -27,7 +22,6 @@ setup(name='pytest-excel',
           'Development Status :: 5 - Production/Stable',
           'Framework :: Pytest',
           'Intended Audience :: Developers',
-          'License :: OSI Approved :: MIT License',
           'Operating System :: POSIX',
           'Operating System :: Microsoft :: Windows',
           'Operating System :: MacOS :: MacOS X',
@@ -38,5 +32,5 @@ setup(name='pytest-excel',
           'Programming Language :: Python :: 3.11',
       ],
       python_requires='>=3.11',
-      )
+)
 
